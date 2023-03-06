c00c3acca6e6e081e19f4e15a1ed674c0384ee83
acid-harry <igorkrivouhov@gmail.com>
2023-03-06 19:27:39 +0300

    duck.java

---
+++ Lesson_2/Duck.java
@@ -0,0 +1,29 @@
+public class Duck extends Animal implements Runable, Flyable, Swimable {
+
+    public Duck(String name, int box) {
+        super(name, box);
+    }
+
+    @Override
+    public int speedofFly() {
+        return 75;
+    }
+
+    @Override
+    public String voice() {
+        return "Крякает";
+    }
+
+    @Override
+    public int speedofRun() {
+        
+        return 5;
+    }
+
+    @Override
+    public int speedofSwim() {
+        
+        return 12;
+    }
+    
+}
