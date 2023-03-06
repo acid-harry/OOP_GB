de0d6258ef6e2f14f989c836a1355e275218da44
acid-harry <igorkrivouhov@gmail.com>
2023-03-06 19:26:09 +0300

    cat.java

---
+++ Lesson_2/Cat.java
@@ -0,0 +1,28 @@
+public class Cat extends Animal implements Runable{
+
+    private String color;
+
+    public Cat(String name, int box, String color) {
+        super(name, box);
+        this.color = color;
+    }
+
+    @Override
+    public String voice() {
+        return "Мяукает";
+    }
+
+    @Override
+    public String toString() {
+        return "Кошка " + super.toString();
+    }
+
+    @Override
+    public int speedofRun() {
+        return 15;
+    }
+
+    
+    
+    
+}
